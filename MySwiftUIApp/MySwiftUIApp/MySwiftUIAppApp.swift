6d46997b8af705ab92102bbb0d69d2a052a13f84
Douglas Amorim <douglasamorim@MacBook-Air-de-Douglas.local>
2023-08-27 11:29:02 -0300

    feat implemented asyncimage usage

---
+++ MySwiftUIApp/MySwiftUIApp/MySwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIAppApp.swift
+//  MySwiftUIApp
+//
+//  Created by Douglas Amorim on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
