85538c7e48e93dbb773e250edf2d51c027ad2f32
Douglas Amorim <amorimdos@gmail.com>
2023-09-25 21:39:09 -0300

    feat drag and drop button with layout changes

--- MySwiftUIApp/MySwiftUIApp/Scenes/OnboardingView.swift
+++ MySwiftUIApp/MySwiftUIApp/Scenes/OnboardingView.swift
@@ -11,6 +11,8 @@ struct OnboardingView: View {
     // MARK: - PROPERTY
     // The 'true' value will only be add on 'onboarding' propertie when the 'onboarding' key previously
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
     
     // MARK: - BODY
     var body: some View {
@@ -70,7 +72,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color.customRed)
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         
                         Spacer()
                     }
@@ -87,14 +89,28 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                }
+                        )//: GESTURE
                         
                         Spacer()
                     }//: HSTACK
                 }//: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             } //: VSTACK
         }//: ZSTACK
