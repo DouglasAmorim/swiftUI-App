decdaa77c5686fb405ebce3721f101d3f0fb9e00
Douglas Amorim <amorimdos@gmail.com>
2023-12-05 10:53:39 -0300

    feat drag gesture implemented

--- MySwiftUIApp/MySwiftUIApp/Scenes/PinchMainView.swift
+++ MySwiftUIApp/MySwiftUIApp/Scenes/PinchMainView.swift
@@ -11,8 +11,15 @@ struct PinchMainView: View {
     // MARK: - Propertie
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - Function
+    private func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - Content
     var body: some View {
@@ -27,19 +34,32 @@ struct PinchMainView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
-                // MARK: - 1 Tap Gesture
+                // MARK: - 1. Tap Gesture
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                // MARK: - 2. Drag Gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
                 // Implicit animation: .animation(.linear(durarion: 1), value: isAnimating)
             } //: ZStack
             .navigationTitle("Pinch & Zoom")
